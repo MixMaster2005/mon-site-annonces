70eb9d219d4b642ad760a9db9d4504b43249fc46
MixMaster2005 <2005aina2005@gmail.com>
2026-04-10 18:40:25 +0300

    test

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
+  base: '/mon-site-annonces/', // 👈 AJOUT ICI
   plugins: [react()],
   resolve: {
     alias: {
